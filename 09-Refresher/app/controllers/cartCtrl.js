b90b8da43c27ebdffb5d68306ffb71c7852da921
Bengt Weiße <bengtler@gmail.com>
2016-03-08 21:25:42 +0100

    add refresher

---
+++ 09-Refresher/app/controllers/cartCtrl.js
@@ -0,0 +1,13 @@
+    angular
+    .module('pizzaApp')
+    .controller('CartCtrl', [
+        '$scope',
+        'cartService',
+        function ($scope, cartService) {
+            $scope.cart = cartService.getCart();
+
+            $scope.removeFromCart = function (index) {
+                cartService.removeCartItem(index);
+            };
+        }
+    ]);
