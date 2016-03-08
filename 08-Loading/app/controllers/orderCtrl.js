cd374b45629c2a7b462264d5ab36d2bea57810d9
Bengt Weiße <bengtler@gmail.com>
2016-03-08 19:37:30 +0100

    add loading code

---
+++ 08-Loading/app/controllers/orderCtrl.js
@@ -0,0 +1,28 @@
+angular
+    .module('pizzaApp')
+    .controller('OrderCtrl', [
+        '$scope',
+        '$timeout',
+        'cartService',
+        'pizzaService',
+        function ($scope, $timeout, cartService, pizzaService) {
+            $scope.loading = true;
+
+            pizzaService
+                .getPizzas()
+                .then(function (result) {
+                    $scope.pizzas = result;
+                }, function () {
+                    // error case
+                })
+                .finally(function () {
+                    $timeout(function () {
+                        $scope.loading = false;
+                    }, 500);
+                });
+
+            $scope.addToCart = function (pizza) {
+                cartService.addCartItem(pizza);
+            };
+        }
+    ]);
