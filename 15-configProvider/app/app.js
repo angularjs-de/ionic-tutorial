60434ca4ff49b60d0b99695b0be27bcfe691c7e2
Bengt Weiße <bengtler@gmail.com>
2016-03-29 16:16:27 +0200

    add configProvider example

---
+++ 15-configProvider/app/app.js
@@ -0,0 +1,40 @@
+angular
+    .module('pizzaApp', ['ionic'])
+    .config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        '$ionicConfigProvider',
+        function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+            $ionicConfigProvider.backButton.text('');
+
+            $urlRouterProvider
+                .otherwise('/order');
+
+            $stateProvider
+                .state('base', {
+                    url: '/',
+                    abstract: true,
+                    views: {
+                        'base': {
+                            templateUrl: 'app/templates/base.html',
+                            controller: 'BaseCtrl'
+                        }
+                    }
+                })
+                .state('base.order', {
+                    url: 'order',
+                    templateUrl: 'app/templates/order.html',
+                    controller: 'OrderCtrl'
+                })
+                .state('base.cart', {
+                    url: 'cart',
+                    templateUrl: 'app/templates/cart.html',
+                    controller: 'CartCtrl'
+                })
+                .state('base.detail', {
+                    url: 'pizza/:id',
+                    templateUrl: 'app/templates/detail.html',
+                    controller: 'DetailCtrl'
+                });
+        }
+    ]);
---
+++ 15-configProvider/app/controllers/orderCtrl.js
@@ -0,0 +1,42 @@
+angular
+    .module('pizzaApp')
+    .controller('OrderCtrl', [
+        '$scope',
+        '$timeout',
+        'cartService',
+        'pizzaService',
+        function ($scope, $timeout, cartService, pizzaService) {
+
+            function loadPizzas(refresh) {
+                if (!refresh) {
+                    $scope.loading = true;
+                }
+                return pizzaService
+                    .getPizzas()
+                    .then(function (result) {
+                        $scope.pizzas = result;
+                    }, function () {
+                        // error case
+                    })
+                    .finally(function () {
+                        // show the spinner a little bit longer
+                        $timeout(function () {
+                            $scope.loading = false;
+                        }, 2000);
+                    });
+            }
+            loadPizzas();
+
+            $scope.refresh = function () {
+                loadPizzas(true)
+                    .finally(function () {
+                        $scope.$broadcast('scroll.refreshComplete');
+                    });
+            };
+
+            $scope.addToCart = function ($event, pizza) {
+                $event.stopImmediatePropagation();
+                cartService.addCartItem(pizza);
+            };
+        }
+    ]);
